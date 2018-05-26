079301548b24f7d9203d24e3cf4ea3ce6f4e3882
Jonathan Lebon <jonathan@jlebon.com>
2018-05-26 15:19:53 -0400

    codebase: use SPDX license identifiers

--- native/textern.py
+++ native/textern.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python3
 # vim: set et ts=4 tw=80:
-#
-# Copyright (C) 2017 Jonathan Lebon
-#
 # This file is part of Textern.
-# Textern is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Textern is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Textern.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) 2017-2018  Jonathan Lebon <jonathan@jlebon.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import os
 import sys
