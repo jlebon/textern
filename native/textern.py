d5e357c0bc3a86f17ba68c27fa28fc65611080f8
Jonathan Lebon <jonathan@jlebon.com>
2022-12-18 10:20:58 -0500

    Add support for a backup directory
    If by accident, one closes the tab or closes Firefox by accident before
    submitting your work, you could lose all the text you've written. Add an
    experimental option to allow copying the text files into a backup
    directory. Backups are updated on every save and kept for 24 hours
    before being pruned.
    
    Closes: #83

--- native/textern.py
+++ native/textern.py
@@ -14,12 +14,16 @@ import sys
 import tempfile
 import urllib.parse
 import datetime
+import time
 
 try:
     from inotify_simple import INotify, flags
 except ImportError:
     # fall-back to the old import pattern for git-submodule use-case (PR#57)
     from inotify_simple.inotify_simple import INotify, flags
+
+
+BACKUP_RETENTION_SECS = 24 * 60 * 60
 
 
 class TmpManager():
@@ -35,6 +39,7 @@ class TmpManager():
             tmpdir_parent = f'/run/textern-{uid}'
             os.makedirs(tmpdir_parent, exist_ok=True)
             os.chmod(tmpdir_parent, 0o700)
+        self._backupdir = ""
         self.tmpdir = tempfile.mkdtemp(prefix=tmpd_prefix, dir=tmpdir_parent)
         self._tmpfiles = {}  # relfn --> opaque
 
@@ -46,6 +51,19 @@ class TmpManager():
 
     def __contains__(self, relfn):
         return relfn in self._tmpfiles
+
+    def update_backupdir(self, val):
+        self._backupdir = val
+        if self._backupdir != "":
+            os.makedirs(self._backupdir, exist_ok=True)
+            now = time.time()
+            for path in os.listdir(self._backupdir):
+                fn = os.path.join(self._backupdir, path)
+                if not os.path.isfile(fn):
+                    continue
+                stbuf = os.stat(fn)
+                if now - stbuf.st_mtime > BACKUP_RETENTION_SECS:
+                    os.unlink(fn)
 
     def new(self, text, url, extension, opaque):
         sanitized_url = urllib.parse.quote(url, safe='')
@@ -67,6 +85,14 @@ class TmpManager():
         assert relfn in self._tmpfiles
         self._tmpfiles.pop(relfn)
         os.unlink(absfn)
+
+    def backup(self, relfn):
+        if self._backupdir == "":
+            return
+        assert relfn in self._tmpfiles
+        absfn = os.path.join(self.tmpdir, relfn)
+        if os.stat(absfn).st_size > 0:
+            shutil.copyfile(absfn, os.path.join(self._backupdir, relfn))
 
     def get(self, relfn):
         assert relfn in self._tmpfiles
@@ -147,7 +173,11 @@ async def handle_message_new_text(tmp_mgr, msg):
     absfn = tmp_mgr.new(msg["text"], msg["url"],
                         msg["prefs"]["extension"], msg["id"])
 
+    # for now, we get preferences as part of new_text updates; in the future, we
+    # may add a `set_prefs` message dedicated to this
+    tmp_mgr.update_backupdir(msg["prefs"].get("backupdir", ""))
     editor_args = json.loads(msg["prefs"]["editor"])
+
     line, column = offset_to_line_and_column(msg["text"], msg["caret"])
 
     editor_args = get_final_editor_args(editor_args, absfn, line, column)
@@ -182,6 +212,7 @@ def handle_inotify_event(ino, tmp_mgr):
         if event.name in tmp_mgr:
             text, id = tmp_mgr.get(event.name)
             send_text_update(id, text)
+            tmp_mgr.backup(event.name)
 
 
 def send_text_update(id, text):
